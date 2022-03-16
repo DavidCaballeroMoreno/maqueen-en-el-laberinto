2fbbdf31a6198a2c1433dde17337f043e5d49e21
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-03-16 09:27:57 +0100

    Casi lo termino

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
+function girar90gradosderecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(260)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+}
 function girar90gradosizquierda () {
     maqueen.motorStop(maqueen.Motors.All)
     basic.pause(260)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
-    basic.pause(500)
+    basic.pause(1000)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
 }
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
         maqueen.motorStop(maqueen.Motors.All)
         girar90gradosizquierda()
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+            maqueen.motorStop(maqueen.Motors.All)
+            girar90gradosderecha()
+            if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+                maqueen.motorStop(maqueen.Motors.All)
+                girar90gradosderecha()
+            }
+        }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
