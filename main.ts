63ab6a1ad1480251731a8ed006e4acfb157f9f7b
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-03-21 11:59:24 +0100

    ¡¡¡Lo he conseguido!!!

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 function girar90gradosderecha () {
     maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(260)
+    basic.pause(220)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-    basic.pause(1000)
+    basic.pause(1300)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
 }
 function girar90gradosizquierda () {
     maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(260)
+    basic.pause(220)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
-    basic.pause(1000)
+    basic.pause(1300)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
 }
 basic.forever(function () {
@@ -24,6 +24,10 @@ basic.forever(function () {
             if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
                 maqueen.motorStop(maqueen.Motors.All)
                 girar90gradosderecha()
+                if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+                    maqueen.motorStop(maqueen.Motors.All)
+                    girar90gradosderecha()
+                }
             }
         }
     } else {
